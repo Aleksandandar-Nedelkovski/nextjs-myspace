c7c03653d3f50db1a4719473f5eebae8113b7fbc
Aleksandandar-Nedelkovski <aleksn.dev@gmail.com>
2023-06-17 12:35:45 -0500

    static generation

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 420; // not necessary, just for ISR (Incremental Static Regeneration) demonstration
+
 interface Post {
   title: string;
   content: string;
@@ -8,8 +10,20 @@ interface Props {
   params: { slug: string };
 }
 
-export default async function BlogPostPage({ params }: Props) {
+export async function generateStaticParams() {
   const posts: Post[] = await fetch('http://localhost:3000/api/content').then(
+    (res) => res.json()
+  );
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+export default async function BlogPostPage({ params }: Props) {
+  const posts: Post[] = await fetch('http://localhost:3000/api/content',
+    { cache: 'no-cache' }
+  ).then(
     (res) => res.json()
   );
   const post = posts.find((post) => post.slug === params.slug)!;
